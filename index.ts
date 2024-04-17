784de156eb1a160889dfa55fd66bbf28fca37d29
Muhammad Rohan Mirza <m.rohanmirza2007@gmail.com>
2024-04-18 01:33:07 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+// import the `inquirer` modules, which is a command line interface for Node.js.
+import inquirer from "inquirer"
+
+// Declare a constant 'answer' and assign it to the result of inquirer.prompt function.
+    
+
+let name: string = "Wellcome to Word counter "
+console.log(name);
+
+const answer = await inquirer.prompt([
+        {
+            name:"sentence",
+            type:"input",
+            message:"To count the words, type your statement here:"
+        }
+])
+
+const words =answer.sentence.trim().split(" ");
+// print the array of words to the console
+console.log(words)
+
+// print the word count of the sentence to the console
+console.log(`The word count of your sentence is: ${words.length}`);
